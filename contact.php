07c42ac2f8070ddf498c19f74cf05d8110792b9a
subarna200 <71552094+subarna200@users.noreply.github.com>
2021-02-16 20:01:12 +0545

    Update contact.php

--- contact.php
+++ contact.php
@@ -34,7 +34,7 @@ if (!empty($_POST)) {
         $body = join(PHP_EOL, $bodyParagraphs);
 
         if (mail($toEmail, $emailSubject, $body, $headers)) {
-            header('Location: thank-you.html');
+            header('Location: index.html');
         } else {
             $errorMessage = 'Oops, something went wrong. Please try again later';
         }
